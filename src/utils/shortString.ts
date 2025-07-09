c5a1ce52d563ef498a10c59cc839f4c13a1427e8
Sisyfos <107465625+0xsisyfos@users.noreply.github.com>
2025-07-09 13:45:31 +0200

    Update shortString.ts

--- src/utils/shortString.ts
+++ src/utils/shortString.ts
@@ -35,4 +35,22 @@ export function decodeShortString(felt: BigNumberish): string {
     bytes.push(parseInt(decoded.substr(i, 2), 16));
   }
   return String.fromCharCode(...bytes);
+}
+
+/**
+ * Test if a string is a Cairo short string (string with less or equal 31 characters)
+ * @param {string} str the string to test
+ * @returns {boolean} Returns true if the string has less than or equal to 31 characters, otherwise false.
+ */
+export function isShortString(str: string): boolean {
+  return str.length <= 31;
+}
+
+/**
+ * Test if value is a pure string text, and not a hex string or number string
+ * @param {any} val the value to test
+ * @returns {boolean} returns true if the value is a free-form string text, otherwise false
+ */
+export function isText(val: any): boolean {
+  return typeof val === 'string' && !/^0x[0-9a-fA-F]+$/.test(val) && !/^[0-9]+$/.test(val);
 } 
