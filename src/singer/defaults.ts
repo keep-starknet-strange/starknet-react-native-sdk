dcf8654ecd6b144408a1f77e2b6d821b439ccd66
Sisyfos <107465625+0xsisyfos@users.noreply.github.com>
2025-07-15 11:19:10 +0200

    Create defaults.ts

---
+++ src/singer/defaults.ts
@@ -0,0 +1,131 @@
+import {
+  Call,
+  DeclareSignerDetails,
+  DeployAccountSignerDetails,
+  InvocationsSignerDetails,
+  Signature,
+  TypedData,
+  V2DeclareSignerDetails,
+  V2DeployAccountSignerDetails,
+  V2InvocationsSignerDetails,
+  V3DeclareSignerDetails,
+  V3DeployAccountSignerDetails,
+  V3InvocationsSignerDetails,
+} from '../types';
+import { ETransactionVersion2, ETransactionVersion3 } from '../types/api';
+import { CallData } from '../utils/calldata/CallData';
+import { starkCurve } from '../utils/ec/starkCurve';
+import { toHex } from '../utils/num';
+import {
+  calculateDeclareTransactionHash,
+  calculateDeployAccountTransactionHash,
+  calculateInvokeTransactionHash,
+} from '../utils/hash/classHash';
+import { intDAM } from '../utils/stark';
+import { getExecuteCalldata } from '../utils/calldata/CallData';
+import { getMessageHash } from '../utils/typedData';
+import { SignerInterface } from './interface';
+
+export class Signer implements SignerInterface {
+  protected pk: Uint8Array | string;
+
+  constructor(pk: Uint8Array | string) {
+    this.pk = pk instanceof Uint8Array ? toHex(pk) : toHex(pk);
+  }
+
+  public async getPubKey(): Promise<string> {
+    return starkCurve.getStarkKey(this.pk);
+  }
+
+  public async signMessage(typedData: TypedData, accountAddress: string): Promise<Signature> {
+    const msgHash = getMessageHash(typedData, accountAddress);
+    return this.signRaw(msgHash);
+  }
+
+  public async signTransaction(
+    transactions: Call[],
+    details: InvocationsSignerDetails
+  ): Promise<Signature> {
+    const compiledCalldata = getExecuteCalldata(transactions, details.cairoVersion);
+    let msgHash;
+    if (Object.values(ETransactionVersion2).includes(details.version as any)) {
+      const det = details as V2InvocationsSignerDetails;
+      msgHash = calculateInvokeTransactionHash({
+        ...det,
+        senderAddress: det.walletAddress,
+        compiledCalldata,
+        version: det.version,
+      });
+    } else if (Object.values(ETransactionVersion3).includes(details.version as any)) {
+      const det = details as V3InvocationsSignerDetails;
+      msgHash = calculateInvokeTransactionHash({
+        ...det,
+        senderAddress: det.walletAddress,
+        compiledCalldata,
+        version: det.version,
+        nonceDataAvailabilityMode: intDAM(det.nonceDataAvailabilityMode),
+        feeDataAvailabilityMode: intDAM(det.feeDataAvailabilityMode),
+      });
+    } else {
+      throw Error('unsupported signTransaction version');
+    }
+    return this.signRaw(msgHash as string);
+  }
+
+  public async signDeployAccountTransaction(
+    details: DeployAccountSignerDetails
+  ): Promise<Signature> {
+    const compiledConstructorCalldata = CallData.compile(details.constructorCalldata);
+    let msgHash;
+    if (Object.values(ETransactionVersion2).includes(details.version as any)) {
+      const det = details as V2DeployAccountSignerDetails;
+      msgHash = calculateDeployAccountTransactionHash({
+        ...det,
+        salt: det.addressSalt,
+        constructorCalldata: compiledConstructorCalldata,
+        version: det.version,
+      });
+    } else if (Object.values(ETransactionVersion3).includes(details.version as any)) {
+      const det = details as V3DeployAccountSignerDetails;
+      msgHash = calculateDeployAccountTransactionHash({
+        ...det,
+        salt: det.addressSalt,
+        compiledConstructorCalldata,
+        version: det.version,
+        nonceDataAvailabilityMode: intDAM(det.nonceDataAvailabilityMode),
+        feeDataAvailabilityMode: intDAM(det.feeDataAvailabilityMode),
+      });
+    } else {
+      throw Error('unsupported signDeployAccountTransaction version');
+    }
+    return this.signRaw(msgHash as string);
+  }
+
+  public async signDeclareTransaction(
+    details: DeclareSignerDetails
+  ): Promise<Signature> {
+    let msgHash;
+    if (Object.values(ETransactionVersion2).includes(details.version as any)) {
+      const det = details as V2DeclareSignerDetails;
+      msgHash = calculateDeclareTransactionHash({
+        ...det,
+        version: det.version,
+      });
+    } else if (Object.values(ETransactionVersion3).includes(details.version as any)) {
+      const det = details as V3DeclareSignerDetails;
+      msgHash = calculateDeclareTransactionHash({
+        ...det,
+        version: det.version,
+        nonceDataAvailabilityMode: intDAM(det.nonceDataAvailabilityMode),
+        feeDataAvailabilityMode: intDAM(det.feeDataAvailabilityMode),
+      });
+    } else {
+      throw Error('unsupported signDeclareTransaction version');
+    }
+    return this.signRaw(msgHash as string);
+  }
+
+  protected async signRaw(msgHash: string): Promise<Signature> {
+    return starkCurve.sign(msgHash, this.pk);
+  }
+} 
