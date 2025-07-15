16bfe77af5b103b1f1b6083573305049b9a3109e
Sisyfos <107465625+0xsisyfos@users.noreply.github.com>
2025-07-15 10:30:25 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,24 @@ export {
 } from './wallets'
 export {CallData} from './utils/calldata/CallData'
 
+// Import components for compatibility with starknet package format
+
+import {RpcProvider} from './core/rpc-provider'
+import {starkCurve} from './utils/ec/starkCurve'
+import {calculateContractAddressFromHash} from './utils/hash/classHash'
+import {StarknetChainId} from './global/constants'
+// TO DO ADD ACCOUNT
+// Export components for compatibility with starknet package format
+export {Account}
+export {RpcProvider}
+export {starkCurve}
+export {calculateContractAddressFromHash}
+export {StarknetChainId}
+
+// Export in the format expected by starknet package
+export const constants = { StarknetChainId };
+export const ec = { starkCurve };
+export const hash = { calculateContractAddressFromHash };
+
 export const VERSION = '0.1.0'
 export const SDK_NAME = 'Starknet React Native SDK' 
